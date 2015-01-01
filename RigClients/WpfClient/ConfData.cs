1ab630e6ec0f1bb008ac87c8c2bad43e6007a409
Darryl Wagoner <darryl@wagoner.me>
2015-01-01 15:53:24 -0600

    Test commit

--- RigClients/WpfClient/ConfData.cs
+++ RigClients/WpfClient/ConfData.cs
@@ -1,6 +1,6 @@
 ﻿#region -- Copyright
 /*
-   Copyright {2014} {Darryl Wagoner DE WA1GON}
+   Copyright 2014 Darryl Wagoner DE WA1GON
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
